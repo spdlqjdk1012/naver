30cde05d00916bf0668f65c378e0498808903b11
None-ryu <koujiro10235@naver.com>
2022-06-23 23:37:55 +0900

    15. 12919

--- 15.12919.py
+++ 15.12919.py
@@ -2,17 +2,22 @@ S = input()
 T = input()
 flag = False
 def change(arr):
-    global T
+    global S
     global flag
-    if len(arr) >= len(T):
-        if arr == T:
+    if len(arr) <= len(S):
+        if arr == S:
             flag = True
         return
-    tmp = arr + "A"
-    change(tmp)
-    arr = arr + "B"
-    change("".join(reversed(arr)))
-change(S)
+    tmp = list(arr)
+    if tmp[-1] == "A":
+        tmp.pop(-1)
+        change(''.join(s for s in tmp))
+    if tmp[0] == "B":
+        arr = list(arr)
+        arr.pop(0)
+        arr = ''.join(s for s in arr)
+        change("".join(reversed(arr)))
+change(T)
 if flag == True:
     print(1)
 else:
